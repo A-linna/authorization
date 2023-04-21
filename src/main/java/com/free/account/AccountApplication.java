e703e4eac8d417f6912b5f86cb603b17659ff1e3
wlf <>
2023-04-21 19:47:38 +0800

    init

---
+++ src/main/java/com/free/account/AccountApplication.java
@@ -0,0 +1,16 @@
+package com.free.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author aiLun
+ * @date 2023/4/21-19:30
+ */
+
+@SpringBootApplication
+public class AccountApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AccountApplication.class);
+    }
+}
